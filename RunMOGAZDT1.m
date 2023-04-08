5227452fde2169148f0d05d69baed362f220913e
matthewmjw22 <90141302+matthewmjw22@users.noreply.github.com>
2023-04-08 07:56:57 -0400

    Add files via upload

--- RunMOGAZDT1.m
+++ RunMOGAZDT1.m
@@ -1,11 +1,22 @@
-syms x
-n=30;
-f1 = x;  % Min
-h = 1 - sqrt(f1/g)
-g = 1 + 9/(n-1) * symsum(x,x,2,n)
-f2 = g*h;   % Min
+% syms x
+% n=30;
+% f1 = x;  % Min
+% h = 1 - sqrt(f1/g)
+% g = 1 + 9/(n-1) * symsum(x,x,2,n)
+% f2 = g*h;   % Min
+% 
+% % X between 0 and 1
+% 
+% 
+% % Call our moga function
 
-% X between 0 and 1
 
 
-% Call our moga function
+n = 30;
+fun = @(x) [x, (1 + 9/(n-1) * sum(x(2:end)) * (1 - sqrt(x/(1 + 9/(n-1) * sum(x(2:end))))))];
+lb = zeros(1, n);
+ub = ones(1, n);
+nonlcon = @(x) deal([], [1 - sqrt(x(1)/(1 + 9/(n-1) * sum(x(2:end))))]);
+
+options = optimoptions('gamultiobj','PopulationSize',100,'MaxGenerations',500,'Display','final');
+[x,fval] = gamultiobj(fun,n,[],[],[],[],lb,ub,nonlcon,options);
