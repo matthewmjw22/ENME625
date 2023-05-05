7a6c1f1cb5dac3a87ea536759c5b813bce3fc3bb
matthewmjw22 <90141302+matthewmjw22@users.noreply.github.com>
2023-05-05 16:10:36 -0400

    Add files via upload

--- TNK/RunMOGA_TNK.m
+++ TNK/RunMOGA_TNK.m
@@ -1,50 +1,26 @@
+
+
 clc
 clear all
 
 nvars = 2;
 lb = [0; 0];
 ub = [pi; pi];
-MaxGenerations_Data = 300;
+MaxGenerations_Data = 200;
 
-[x,fval,exitflag,output,population,score] = gammultiobjsolverTNK(nvars,lb,ub,MaxGenerations_Data);
+[X,fval,exitflag,output,population,score] = gammultiobjsolverTNK(nvars,lb,ub,MaxGenerations_Data);
 
 fval
-x
+X
 
+pareto_spread = Pareto_Spread(fval);
+pareto_spread
+clus = cluster(fval, 100);
+clus
 
-% Quality Metrics
-
-% Pareto Spread 
-% extract x and y values
-x = fval(:,1);
-y = fval(:,2);
-
-% calculate range of x and y values
-x_range = max(x) - min(x);
-y_range = max(y) - min(y);
-
-% calculate maximum range and Pareto spread
-pareto_spread =x_range*y_range
-
-
-% Cluster(CLu) 
-% cluster = number of the observed Pareto solutions/he number of distinct choices
-
-
-
-% assume your array is called x
-num_rows = size(x, 1); % get the number of rows in x
-unique_rows = zeros(num_rows, 1); % initialize a vector to store the number of unique rows
-
-for i = 1:num_rows
-    % get the unique row
-    unique_row = unique(x(i, :));
-    
-    % check if the unique row is the same as the original row
-    if isequal(unique_row, x(i, :))
-        unique_rows(i) = 1; % if the row is unique, set the corresponding element in unique_rows to 1
-    end
+for i = 1:size(population, 1)
+    values(:, i) = objective_functionRunMOGA_TNK(population(i, :));
 end
 
-num_distinct = sum(unique_rows); % get the total number of distinct rows
-Cluster = num_rows/num_distinct
+OS = (max(values(1, :)) - min(values(1,:)))*(max(values(2, :)) - min(values(2,:)));
+pareto_spread/OS
