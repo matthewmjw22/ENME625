697483807f6341b8a78fe18348b48611ccfe9674
matthewmjw22 <90141302+matthewmjw22@users.noreply.github.com>
2023-05-05 16:15:17 -0400

    Delete Fitness_OSY.m

--- OSY/Fitness_OSY.m
+++
@@ -1,6 +0,0 @@
-function Fitness = Fitness_OSY(x)
-fvals = objective_functionRunMOGA_OSY(x);
-rank = non_dominated_sort(fvals);
-[dist, niche, Fi] = calc_rank_distances(x, fvals, rank, .75, 1, .25);
-constraints = nonlinear_constraintsRunMOGA_OSY(x);
-Fitness = -Fi + sum(1000000000000*constraints(constraints>0));
