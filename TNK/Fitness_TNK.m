70e761aafdc4d911981094fe9dab3836ce9119b2
matthewmjw22 <90141302+matthewmjw22@users.noreply.github.com>
2023-05-05 16:16:53 -0400

    Delete Fitness_TNK.m

--- TNK/Fitness_TNK.m
+++
@@ -1,6 +0,0 @@
-function Fitness = Fitness_TNK(x)
-fvals = objective_functionRunMOGA_TNK(x);
-rank = non_dominated_sort(fvals);
-[dist, niche, Fi] = calc_rank_distances(x, fvals, rank, .75, 1, .25);
-constraints = nonlinear_constraintsRunMOGA_TNK(x);
-Fitness = -Fi + sum(1000000000000*constraints(constraints>0));
