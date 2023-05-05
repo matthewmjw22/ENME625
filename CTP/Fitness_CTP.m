703e7b19f4d134eb0b1f2d537bd80d7104b5d664
matthewmjw22 <90141302+matthewmjw22@users.noreply.github.com>
2023-05-05 16:15:06 -0400

    Delete Fitness_CTP.m

--- CTP/Fitness_CTP.m
+++
@@ -1,6 +0,0 @@
-function Fitness = Fitness_CTP(x)
-fvals = objective_functionCTP_batch(x);
-rank = non_dominated_sort(fvals);
-[dist, niche, Fi] = calc_rank_distances(x, fvals, rank, .75, 1, .25);
-constraints = nonlinear_constraintsRunMOGA_CTP(x);
-Fitness = -Fi + sum(1000000000000*constraints(constraints>0));
