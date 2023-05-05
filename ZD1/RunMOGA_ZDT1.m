29bb77f46562a9aab5f9ce2078a1782e86871e78
matthewmjw22 <90141302+matthewmjw22@users.noreply.github.com>
2023-05-05 16:05:15 -0400

    Add files via upload

--- ZD1/RunMOGA_ZDT1.m
+++ ZD1/RunMOGA_ZDT1.m
@@ -1,12 +1,24 @@
 clc
 clear all
 
-nvars = 1;
-lb = 0;
-ub = 1;
-MaxGenerations_Data = 100;
+nvars = 30;
+lb = zeros(30, 1);
+ub = ones(30, 1);
+MaxGenerations_Data = 10000;
 
 [x,fval,exitflag,output,population,score] = gamultiobjsolver_ZDT1(nvars,lb,ub,MaxGenerations_Data);
 
 fval
 x
+
+pareto_spread = Pareto_Spread(fval);
+pareto_spread
+clus = cluster(fval, 100);
+clus
+
+for i = 1:size(population, 1)
+    values(:, i) = objective_functionZDT1(population(i, :));
+end
+
+OS = (max(values(1, :)) - min(values(1,:)))*(max(values(2, :)) - min(values(2,:)));
+pareto_spread/OS
