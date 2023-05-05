ecbdd492f937e175e125594d43eac4d8d583ab5d
matthewmjw22 <90141302+matthewmjw22@users.noreply.github.com>
2023-05-05 16:12:01 -0400

    Add files via upload

--- ZD2/RunMOGA_ZDT2.m
+++ ZD2/RunMOGA_ZDT2.m
@@ -1,10 +1,10 @@
 clc
 clear all
 
-nvars = 1;
-lb = 0;
-ub = 1;
-MaxGenerations_Data = 100;
+nvars = 30;
+lb = zeros(30, 1);
+ub = ones(30, 1);
+MaxGenerations_Data = 100000;
 
 [x,fval,exitflag,output,population,score] = gamultiobjsolver_ZDT2(nvars,lb,ub,MaxGenerations_Data);
 
@@ -12,39 +12,14 @@ fval
 x
 
 
-% Quality Metrics
+pareto_spread = Pareto_Spread(fval);
+pareto_spread
+clus = cluster(fval, 100);
+clus
 
-% Pareto Spread 
-% extract x and y values
-x = fval(:,1);
-y = fval(:,2);
-
-% calculate range of x and y values
-x_range = max(x) - min(x);
-y_range = max(y) - min(y);
-
-% calculate maximum range and Pareto spread
-pareto_spread =x_range*y_range
-
-
-% Cluster(CLu) 
-% cluster = number of the observed Pareto solutions/he number of distinct choices
-
-
-
-% assume your array is called x
-num_rows = size(x, 1); % get the number of rows in x
-unique_rows = zeros(num_rows, 1); % initialize a vector to store the number of unique rows
-
-for i = 1:num_rows
-    % get the unique row
-    unique_row = unique(x(i, :));
-    
-    % check if the unique row is the same as the original row
-    if isequal(unique_row, x(i, :))
-        unique_rows(i) = 1; % if the row is unique, set the corresponding element in unique_rows to 1
-    end
+for i = 1:size(population, 1)
+    values(:, i) = objective_functionZDT2(population(i, :));
 end
 
-num_distinct = sum(unique_rows); % get the total number of distinct rows
-Cluster = num_rows/num_distinct
+OS = (max(values(1, :)) - min(values(1,:)))*(max(values(2, :)) - min(values(2,:)));
+pareto_spread/OS
